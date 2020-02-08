15d0ac8e85639ee4c5043b0b388d66c0ad27a783
Kobi Mantzur <kobimantzur@gmail.com>
2020-02-08 20:58:54 +0200

    .

--- src/components/About/Components/GetTheAppSection/index.js
+++ src/components/About/Components/GetTheAppSection/index.js
@@ -13,10 +13,10 @@ export default class GetTheAppSection extends Component {
         </p>
         </div>
         <div className="left">
-          <a href="">
+          <a href="https://itunes.apple.com/us/app/avartii-%D7%99%D7%A9%D7%A8%D7%90%D7%9C%D7%99%D7%9D-%D7%91%D7%9C%D7%95%D7%A1-%D7%90%D7%A0%D7%92-%D7%9C%D7%A1/id1329505993?mt=8">
           <img className="app-store" src="/static/img/download/app-store-download.svg" />
           </a>
-          <a href="">
+          <a href="https://play.google.com/store/apps/details?id=com.avartii.app">
           <img className="play-store" src="/static/img/download/google-play-download.svg" />
           </a>
         </div>
